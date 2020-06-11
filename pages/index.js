76dbbd19d26e16a9d3e518ae547eff3189bf9fa2
arzafran <franco@basement.studio>
2020-06-11 10:53:16 -0300

    basic boilerplate

---
+++ pages/index.js
@@ -0,0 +1,161 @@
+import Link from "next/link"
+import Layout from "components/Layout"
+import Collapsible from "components/Collapsible"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to my quick Locomotive scroll starter.</p>
+    <h2>Locomotive Scroll and innerHeight</h2>
+    <p>
+      Any component that changes the innerHeight of the page should trigger the
+      update method.
+    </p>
+    <p>
+      In our case <code>window.scroll.update()</code>.{" "}
+      <a href="">See the `collapsible` component</a>.
+    </p>
+    <Collapsible>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </Collapsible>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. A libero
+      veritatis totam, iste hic quibusdam doloribus, odio aliquid molestiae sunt
+      rem. Minima, adipisci modi! Cumque aperiam delectus pariatur quia quo.
+    </p>
+  </Layout>
+)
+
+export default IndexPage
