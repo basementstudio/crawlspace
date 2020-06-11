7bac51b3f3697d91277b14fce15c6fcdd211fcf4
arzafran <franco@basement.studio>
2020-06-11 11:01:18 -0300

    fix gsap import

--- components/Collapsible/index.js
+++ components/Collapsible/index.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react"
-import { gsap } from "gsap/all"
+import gsap from "gsap"
 import { isBrowser } from "react-device-detect"
 import styles from "./collapsible.module.css"
 
